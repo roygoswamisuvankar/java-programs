b5c94f5fa3f69fa156b5ae963d26bc107aa0c34c
suvankar roy goswami <46588797+roygoswamisuvankar@users.noreply.github.com>
2021-05-11 18:32:05 +0530

    Create question_5.java

---
+++ java_assign/question_5.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+class calculate{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of rows: ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=n; j>=i; j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+Enter no of rows:
+5
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+ */
