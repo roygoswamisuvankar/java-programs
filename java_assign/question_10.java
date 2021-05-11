95d3c67f86a719cc200778ca63abfccac702ee8a
suvankar roy goswami <46588797+roygoswamisuvankar@users.noreply.github.com>
2021-05-11 19:40:25 +0530

    Create question_10.java

---
+++ java_assign/question_10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class calculate{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a charecter: ");
+        char n = sc.next().charAt(0);
+        System.out.println("The ASCII value: "+((int) n));
+    }
+}
